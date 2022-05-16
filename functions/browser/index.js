28b8dde2f6a42a720e285beae57f9996cc605017
MohamadObeid <komikbenim@gmail.com>
2022-05-16 14:56:25 +0300

    16 May

--- functions/browser/index.js
+++ functions/browser/index.js
@@ -1434,7 +1434,7 @@ const { toCode } = require("./toCode")
 //
 require('dotenv').config()
 
-const createDocument = async ({ req, res, db }) => {
+const createDocument = async ({ req, res, db, realtimedb }) => {
 
     // Create a cookies object
     var host = req.headers["x-forwarded-host"] || req.headers["host"]
@@ -1443,10 +1443,11 @@ const createDocument = async ({ req, res, db }) => {
     var currentPage = req.url.split("/")[1] || ""
     currentPage = currentPage || "main"
     
-    var promises = [], user, page, view, project
+    var user, page, view, project
     
     // get assets & views
     var global = {
+        timer: new Date().getTime(),
         data: {
             user: {},
             view: {},
@@ -1486,42 +1487,75 @@ const createDocument = async ({ req, res, db }) => {
     var bracketDomains = [
         "bracketjs.com",
         "localhost",
-        "bracketjs.localhost"
+        "bracket.localhost"
     ]
 
     // is brakcet domain
     var isBracket = bracketDomains.includes(host)
-    
-    // get project data
-    project = db
-    .collection("_project_").where("domains", "array-contains", host)
-    .get().then(doc => {
 
-        if (doc.docs[0] && doc.docs[0].exists)
-        global.data.project = project = doc.docs[0].data()
-    })
-
-    promises.push(project)
-    await Promise.all(promises)
+    console.log("Document started loading:");
     
+    console.log("before project", new Date().getTime() - global.timer);
+    if (isBracket) {
+        // get page
+        global.data.project = project = Object.values(getJsonFiles({ search: { collection: "_project_", fields: { domains: { "array-contains": host } } } }))[0]
+        
+    } else {
+        page = db
+        .collection(`page-bracket`)
+        .get().then(q => {
+                q.forEach(doc => {
+                global.data.page[doc.id] = doc.data() 
+            })
+            console.log("after page", new Date().getTime() - global.timer);
+        })
+        // get project data
+        project = db
+        .collection("_project_").where("domains", "array-contains", host)
+        .get().then(doc => {
+
+            if (doc.docs[0] && doc.docs[0].exists)
+            global.data.project = project = doc.docs[0].data()
+            console.log("after project", new Date().getTime() - global.timer);
+        })
+    }
+
+    await Promise.resolve(project)
+
     // project not found
     if (!project) return res.send("Project not found!")
     global.projectId = project.id
     
     if (isBracket) {
-
-        // get user (for bracket users only)
-        user = db
-        .collection("_user_").where("projects", "array-contains", project.id)
-        .get().then(doc => {
-            
-            if (doc.docs[0].exists)
-            global.data.user = user = doc.docs[0].data()
-        })
         
+        if (isBracket) {
+
+            console.log("before user", new Date().getTime() - global.timer);
+            global.data.user = user = Object.values(getJsonFiles({ search: { collection: "_user_", fields: { projects: { "array-contains": project.id } } } }))[0]
+            console.log("after user", new Date().getTime() - global.timer);
+
+        } else {
+
+            // get user (for bracket users only)
+            console.log("before user / firestore", new Date().getTime() - global.timer);
+
+            user = db
+            .collection("_user_").where("projects", "array-contains", project.id)
+            .get().then(doc => {
+                
+                if (doc.docs[0].exists)
+                global.data.user = user = doc.docs[0].data()
+                console.log("after user", new Date().getTime() - global.timer);
+            })
+        }
+        
+        console.log("before page", new Date().getTime() - global.timer);
+
         // get page
         global.data.page = page = getJsonFiles({ search: { collection: `page-${project.id}` } })
         
+        console.log("before view", new Date().getTime() - global.timer);
+
         // get view
         global.data.view = view = getJsonFiles({ search: { collection: `view-${project.id}` } })
         
@@ -1530,26 +1564,59 @@ const createDocument = async ({ req, res, db }) => {
         // do not send project details
         delete global.data.project
 
+        console.log("before user", new Date().getTime() - global.timer);
+
+        // do not send user details
+        console.log("after user", new Date().getTime() - global.timer);
+        
+        console.log("before page / firestore", new Date().getTime() - global.timer);
+
         // get page
+        /*
+        page = realtimedb.ref(`page-${project.id}`).once("value").then(snapshot => {
+            global.data.page = snapshot.val()
+            console.log("after page", new Date().getTime() - global.timer);
+        })
+        */
         page = db
-            .collection(`page-${project.id}`)
-            .get().then(q => q.forEach(doc => global.data.page[doc.id] = doc.data() ))
+        .collection(`page-${project.id}`)
+        .get().then(q => {
+                q.forEach(doc => {
+                global.data.page[doc.id] = doc.data() 
+            })
+            console.log("after page", new Date().getTime() - global.timer);
+        })
+
+        console.log("before view / firestore", new Date().getTime() - global.timer);
 
         // get view
+        /*
+        view = realtimedb.ref(`view-${project.id}`).once("value").then(snapshot => {
+            global.data.view = snapshot.val()
+            console.log("after view", new Date().getTime() - global.timer);
+        })
+        */
         view = db
-            .collection(`view-${project.id}`)
-            .get().then(q => q.forEach(doc => global.data.view[doc.id] = doc.data()))
+        .collection(`view-${project.id}`)
+        .get().then(q => {
+                q.forEach(doc => {
+                global.data.view[doc.id] = doc.data()
+            })
+            console.log("after view", new Date().getTime() - global.timer);
+        })
     }
-
-    promises.push(page)
-    promises.push(view)
-    promises.push(user)
     
-    await Promise.all(promises)
+    await Promise.resolve(page)
+    await Promise.resolve(view)
+    await Promise.resolve(user)
+    
+    console.log("Document Ready.");
+    // realtimedb.ref("view-alsabil-tourism").set(global.data.view)
+    // realtimedb.ref("page-alsabil-tourism").set(global.data.page)
     
     // user not found
-    if (!global.data.user) return res.send("User not found")
-    global.data.user = user = global.data.user
+    if (!global.data.user) return res.send("User not found!")
+    user = global.data.user
     
     // page doesnot exist
     if (!global.data.page[currentPage]) return res.send("Page not found!")
@@ -1591,6 +1658,7 @@ const createDocument = async ({ req, res, db }) => {
     var innerHTML = ""
     innerHTML = createElement({ _window, id: "root", req, res })
     innerHTML += createElement({ _window, id: "public", req, res })
+
     global.idList = innerHTML.split("id='").slice(1).map(id => id.split("'")[0])
 
     // meta
@@ -1603,7 +1671,7 @@ const createDocument = async ({ req, res, db }) => {
     // language
     var language = global.data.page[currentPage].language || "en"
     var direction = (language === "ar" || language === "fa") ? "rtl" : "ltr"
-    
+
     res.send(
     `<!DOCTYPE html>
     <html lang="${language}" dir="${direction}" class="html">
